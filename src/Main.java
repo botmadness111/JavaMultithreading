32c40aed4fc5626a2ca3748c4ae101e491abf652
qqAndrey <63346175+botmadness111@users.noreply.github.com>
2024-02-23 14:56:45 +0300

    Multithreading + Interrupt
    метод thread.interrupt() - позволяет прервать поток thread. Только прерывание происхдит "мягко". Он не сразу отрубается, а ищет точку, где ему можно остановиться и преркратить работу, чтобы ничего не сломалось
    
    Такую точку мы задаем сами, использовав метод: Thread.currentThread().isInterrupted(), который проверяет поток прерван или нет и возвращает true/false.
    
    Т.е. у каждого потока есть переменная, отвечающая за состояние потока - прерван он или нет.
    
    Иключение InterruptedException вызывается в потоке, который был прерван(interrupt). И та же команла sleep не сработает. Это логично, потому что зачем спать в потоке, который был прерван...
    Иключение InterruptedException меняет состояние потока interrupt на false. Поэтому нужно будет еще раз вызвать Thread.currentThread().interrupt();

--- src/Main.java
+++ src/Main.java
@@ -8,106 +8,34 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-
-        Runner runner = new Runner();
-
-        Thread thread1 = new Thread(new Runnable() {
+        Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                runner.firstThread();
+                System.out.println("Start thread");
+                for (int i = 0; i < 1_000_000_000; i++) {
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        System.out.println("in catch of InterruptedException");
+                        Thread.currentThread().interrupt();
+                    }
+
+                    if (Thread.currentThread().isInterrupted()){
+                        break;
+                    }
+
+                    Math.sin(i);
+                }
+
             }
         });
 
-        Thread thread2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                runner.secondThread();
-            }
-        });
+        thread.start();
+
+        thread.interrupt(); //меняем состояние потока на 'прерванный'
+        thread.join();
 
 
-        thread1.start();
-        thread2.start();
-
-        thread1.join();
-        thread2.join();
-
-        runner.info();
-    }
-}
-
-class Runner {
-    Account account1 = new Account();
-    Account account2 = new Account();
-
-    Lock lock1 = new ReentrantLock();
-    Lock lock2 = new ReentrantLock();
-
-    Random random = new Random();
-
-    public void firstThread() {
-        for (int i = 0; i < 1000; i++) {
-            takeLocks();
-            try {
-                Account.transfer(account1, account2, random.nextInt(10));
-            } finally {
-                lock1.unlock();
-                lock2.unlock();
-            }
-        }
-    }
-
-    public void secondThread() {
-        for (int i = 0; i < 1000; i++) {
-            takeLocks();
-            try {
-                Account.transfer(account2, account1, random.nextInt(10));
-            } finally {
-                lock1.unlock();
-                lock2.unlock();
-            }
-        }
-    }
-
-    private void takeLocks() {
-        boolean firstLockIsTaken = false;
-        boolean secondLockIsTaken = false;
-
-        while (true) {
-            try {
-                firstLockIsTaken = lock1.tryLock();
-                secondLockIsTaken = lock2.tryLock();
-            } finally {
-                if (firstLockIsTaken && secondLockIsTaken) //если удалось взять первый и второй Lock
-                    return;
-
-                if (firstLockIsTaken) {
-                    lock1.unlock();
-                }
-
-                if (secondLockIsTaken) {
-                    lock2.unlock();
-                }
-            }
-        }
-    }
-
-    public void info() {
-        System.out.println("First Account: " + account1.getCash());
-        System.out.println("Second Account: " + account2.getCash());
-        System.out.println("Summary Accounts: " + (account1.getCash() + account2.getCash()));
-    }
-}
-
-class Account {
-    private int cash = 10000;
-
-    public static void transfer(Account account1, Account account2, int value) {
-        account1.cash -= value;
-        account2.cash += value;
-    }
-
-    public int getCash() {
-        return cash;
+        System.out.println("Hello");
     }
 }
