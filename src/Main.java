b568b363f875ab3d23bb723300bf5f899dee0248
qqAndrey <63346175+botmadness111@users.noreply.github.com>
2024-02-23 13:38:15 +0300

    Multithreading + DeadLock
    deadlock - состояние, когда первый поток заблокировал lock1 а второй поток заблокировал lock2
    и блок:
    lock1.lock()
    "*"
    lock2.lock()
    завис на месте "*".
    
    Чтобы решить эту проблему, нужно освободить один из lock.
    
    в методе takeLocks пропишем следующую логику:
    1)если удалось захватить lock1 и lock2 -> выполняем работу
    2)иначе если удалось захватить только один из lock, то unlock этот lock, чтобы другой поток мог его залокать.

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,113 @@
+import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        ExecutorService executorService = Executors.newFixedThreadPool(200);
 
-        Connection connection = Connection.getConnection();
-        for (int i = 0; i < 200; i++) {
-            executorService.submit(new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        connection.work();
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            });
-        }
+        Runner runner = new Runner();
 
-        executorService.shutdown();
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                runner.firstThread();
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                runner.secondThread();
+            }
+        });
+
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        runner.info();
     }
 }
 
-class Connection {
-    private static Connection connection = new Connection();
-    private int connectionCount;
-    private Semaphore semaphore = new Semaphore(10);
+class Runner {
+    Account account1 = new Account();
+    Account account2 = new Account();
 
-    private Connection() {
-    }
+    Lock lock1 = new ReentrantLock();
+    Lock lock2 = new ReentrantLock();
 
-    public static Connection getConnection() {
-        return connection;
-    }
+    Random random = new Random();
 
-    public void work() throws InterruptedException {
-        semaphore.acquire();
-        try {
-            doWork();
-        } finally {
-            semaphore.release();
+    public void firstThread() {
+        for (int i = 0; i < 1000; i++) {
+            takeLocks();
+            try {
+                Account.transfer(account1, account2, random.nextInt(10));
+            } finally {
+                lock1.unlock();
+                lock2.unlock();
+            }
         }
     }
 
-    private void doWork() throws InterruptedException {
-        synchronized (this) {
-            connectionCount++;
-            System.out.println("Work: " + connectionCount);
+    public void secondThread() {
+        for (int i = 0; i < 1000; i++) {
+            takeLocks();
+            try {
+                Account.transfer(account2, account1, random.nextInt(10));
+            } finally {
+                lock1.unlock();
+                lock2.unlock();
+            }
         }
-        Thread.sleep(1000);
-        synchronized (this) {
-            connectionCount--;
+    }
+
+    private void takeLocks() {
+        boolean firstLockIsTaken = false;
+        boolean secondLockIsTaken = false;
+
+        while (true) {
+            try {
+                firstLockIsTaken = lock1.tryLock();
+                secondLockIsTaken = lock2.tryLock();
+            } finally {
+                if (firstLockIsTaken && secondLockIsTaken) //если удалось взять первый и второй Lock
+                    return;
+
+                if (firstLockIsTaken) {
+                    lock1.unlock();
+                }
+
+                if (secondLockIsTaken) {
+                    lock2.unlock();
+                }
+            }
         }
+    }
+
+    public void info() {
+        System.out.println("First Account: " + account1.getCash());
+        System.out.println("Second Account: " + account2.getCash());
+        System.out.println("Summary Accounts: " + (account1.getCash() + account2.getCash()));
+    }
+}
+
+class Account {
+    private int cash = 10000;
+
+    public static void transfer(Account account1, Account account2, int value) {
+        account1.cash -= value;
+        account2.cash += value;
+    }
+
+    public int getCash() {
+        return cash;
     }
 }
